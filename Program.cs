8675f33cc2f7925214a500d0d7ce5ec2ac37add3
Thomaz Saito <thomaz.saito@codeblox.com.br>
2020-12-09 14:22:43 -0300

    adding .gitignore, Program.cs and project files (.sln, .csproj); editing README.md;

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatrSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sp = new ServiceCollection()
+            .AddMediatR(typeof(Program))
+            .AddScoped<IPipelineBehavior<SaveCommand, string>, SomeBehavior>()
+            .AddSingleton<SomeController>()
+            .BuildServiceProvider();
+
+            var someController = sp.GetService<SomeController>();
+            var someControllerResult = someController.Save(new SaveCommand
+            {
+                Thing = "something..."
+            });
+
+            Console.WriteLine(someControllerResult);
+        }
+    }
+
+    class SomeController
+    {
+        readonly ISender sender;
+
+        public SomeController(ISender sender)
+        {
+            this.sender = sender;
+        }
+
+        public string Save(SaveCommand thing)
+        {
+            return sender.Send(thing).Result;
+        }
+    }
+
+    class SaveCommand : IRequest<string>
+    {
+        public string Thing;
+    }
+
+    class SaveCommandHandler : IRequestHandler<SaveCommand, string>
+    {
+        public Task<string> Handle(SaveCommand request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Saving {request.Thing}");
+            return Task.FromResult("Saved");
+        }
+    }
+
+    class SomeBehavior : IPipelineBehavior<SaveCommand, string>
+    {
+        public async Task<string> Handle(SaveCommand request, CancellationToken cancellationToken, RequestHandlerDelegate<string> next)
+        {
+            request.Thing += " (injected by behavior)";
+            var result = await next();
+            result += " (result manipulation)";
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mediatr-sample
+
+A simple C# console program to demonstrate the usage of request, handlers and (pipeline) behaviors of [jbogards's Mediatr](https://github.com/jbogard/MediatR) library.
+
+### Running the sample
+
+Execute the command `dotnet run` in the project's root folder, provided you have [dotnet](https://dotnet.microsoft.com/download) installed.
+
+### Further reading
+
+MediaR's library is a great resource when you are trying to build an application with the CQRS pattern. Martin Fowler has a [great article](https://martinfowler.com/bliki/CQRS.html) about this subject.
+
+### Contributions
+Contributions to this repo are welcome, specially concerning other aspects of the MediatR library, such as notifications and events.
